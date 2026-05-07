af881def7e6842464ccb7b06f316406708c9adf7
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-05-07 10:53:26 +0200

    initial commit

---
+++ todos.py
@@ -0,0 +1,8 @@
+# TODO 1: Make basic mechanics to read in documents that can be converted to a dataframe (pandas):
+    # TODO 1.1: Make core mechanics for reading file and converting to df in cli
+    # TODO 1.2: Build out relevant ui components
+    # TODO 1.3: Start implementing translation brain (Better to start this early so ui can be built accordingly)
+# TODO 2: Expand df mechanics:
+    # TODO 2.1: Get all the column names of the df
+    # TODO 2.2: Start building analysis functionality
+    # TODO 2.3: Add relevant ui components
