2caf54a5522fbd2b6e51f18dd8595327a7e985b3
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-05-08 13:28:49 +0200

    adjust stats buttons functionality

--- v1/analysis_buttons.py
+++ v1/analysis_buttons.py
@@ -14,6 +14,16 @@ def create_button(operation, root, lang_center, analysis_brain, cols_selected):
     return button
 
 
+def reset_button_text(button, text, analysis_brain):
+    if button.cget('text') == text:
+        button.config(text='HIDE')
+        if analysis_brain.stats_canvas is not None:
+            analysis_brain.stats_canvas = None
+    else:
+        button.config(text=text)
+
+
+
 # 'get_lowest_value', 'unique_values'
 def show_stat(button_id, context):
     analysis_brain = context['analysis_brain']
@@ -31,28 +41,36 @@ def show_stat(button_id, context):
 
 
     if button_id == 'get_highest_value':
-        children = analysis_brain.stats_canvas.winfo_children()
-        for child in children:
-            if isinstance(child, ttk.Label):
-                child.destroy()
+
+        reset_button_text(button, f'{lang_center.translate("get_highest_value")}', analysis_brain)
+
+        if analysis_brain.stats_canvas is not None:
+            children = analysis_brain.stats_canvas.winfo_children()
+            for child in children:
+                if isinstance(child, ttk.Label):
+                    child.destroy()
 
         highest = analysis_brain.get_highest_value(cols_selected)
         label = ttk.Label(stats_canvas, text=f'{lang_center.translate("The highest value in column ")} {cols_selected}: {highest}')
         label.grid(column=0, row=0)
-        button.config(text="HIDE")
+
 
     elif button_id == 'get_lowest_value':
-        children = analysis_brain.stats_canvas.winfo_children()
-        for child in children:
-            if isinstance(child, ttk.Label):
-                child.destroy()
-        lowest = analysis_brain.get_lowest_value(cols_selected)
-        label = ttk.Label(stats_canvas, text=f"{lang_center.translate('The lowest value in column ')} {cols_selected}: {lowest}")
-        label.grid(column=0, row=0)
-        button.config(text="HIDE")
+        reset_button_text(button, f'{lang_center.translate("get_lowest_value")}', analysis_brain)
+
+        if analysis_brain.stats_canvas is not None:
+            children = analysis_brain.stats_canvas.winfo_children()
+            for child in children:
+                if isinstance(child, ttk.Label):
+                    child.destroy()
+            lowest = analysis_brain.get_lowest_value(cols_selected)
+            label = ttk.Label(stats_canvas, text=f"{lang_center.translate('The lowest value in column ')} {cols_selected}: {lowest}")
+            label.grid(column=0, row=0)
 
 
     elif button_id == 'unique_values':
+        reset_button_text(button, f'{lang_center.translate("unique_values")}', analysis_brain)
+
         children = analysis_brain.stats_canvas.winfo_children()
         for child in children:
             if isinstance(child, ttk.Label):
